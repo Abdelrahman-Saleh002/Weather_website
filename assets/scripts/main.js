896074cab95cc6697ef69844b624414e5e2dec13
Abdelrahman Saleh <abdelrahmanisaleh19@gmail.com>
2025-05-12 09:12:37 -0700

    finish the app Logic

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,34 +11,56 @@ const form = document.querySelector("form");
 form.addEventListener("submit" , searchForLocation)
 
 
-let target = "Dubai"
+let target = ""
 
 const fetchResults =async (targetLocation) => {
     let url = `http://api.weatherapi.com/v1/current.json?key=b6c9e8a6ed764d3cac5152643251105&q=${targetLocation}&aqi=no`
     const res = await fetch (url)
     const data = await res.json()
     console.log(data);
-    
-    let temp = data.current.temp_c
-    let locationName = data.location.name
+
+    let locationName = data.location.name    
     let time = data.location.localtime
+    let temp = data.current.temp_c
     let condition = data.current.condition.text
     updateDetails (temp , locationName , time , condition)
 }
 
 function updateDetails (temp , locationName , time , condition){
-    temperatureField.innerHTML = temp
-    locationField.innerHTML = locationName
-    dateandtimeField.innerHTML = time
-    conditionField.innerHTML = condition
+    let splitDate = time.split(' ')[0]
+    let splitTime = time.split(' ')[1]
+    let currentDay = getDayName(new Date(splitDate).getDay()) 
+    temperatureField.innerText = temp
+    locationField.innerText = locationName
+    dateandtimeField.innerText = `${splitDate} ${currentDay} ${splitTime}`
+    conditionField.innerText = condition
 }
 
 
 function searchForLocation(e){
-    e.preventDefualt()
+    e.preventDefault()
     target = searchField.value
     fetchResults(target)
 }
 
 
 fetchResults(target)
+
+function getDayName(number){
+    switch (number) {
+        case 0:
+            return "Sunday";
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
+    }
+}
